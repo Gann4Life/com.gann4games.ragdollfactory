a9e744b0dfbdd178f88838b3d609907b22f6f0e8
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2023-07-06 11:02:02 -0300

    Add source code

---
+++ Gann4Games/CollisionBuilder/CollisionBuilder.MenuItems.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gann4Games.CollisionBuilder
+{
+    public partial class CollisionBuilder
+    {
+        [MenuItem("GameObject/MrGann/Collision Builder", false, priority = 1)]
+        private static void CreateCollisionBuilder(MenuCommand menuCommand)
+        {
+            GameObject obj = Selection.activeObject as GameObject;
+            obj.AddComponent<CollisionBuilder>();
+            Undo.RegisterCreatedObjectUndo(obj, "Created Collision Builder");
+        }
+
+        [MenuItem("GameObject/MrGann/Collision Builder", true, priority = 1)]
+        private static bool CreateCollisionBuilderValidation(MenuCommand menuCommand)
+        {
+            return Selection.activeObject is GameObject &&
+                   !((GameObject)Selection.activeObject).GetComponent<CollisionBuilder>();
+        }
+    }
+}
